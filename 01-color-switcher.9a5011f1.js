7054e8d31f5a030af16e6abfe70948b8ad1e08de
Y0less <125226395+Y0less@users.noreply.github.com>
2023-07-05 22:19:48 +0000

    Deploying to gh-pages from @ Y0less/goit-js-hw-09@a25fc24148647747274ca35012ef2a9b6a6bf786 🚀

---
+++ 01-color-switcher.9a5011f1.js
@@ -0,0 +1,2 @@
+!function(){console.log("Hello!");var t={startButton:document.querySelector("[data-start]"),stopButton:document.querySelector("[data-stop]")};t.startButton.addEventListener("click",(function(){})),t.stopButton.addEventListener("click",(function(){}))}();
+//# sourceMappingURL=01-color-switcher.9a5011f1.js.map
---
+++ 01-color-switcher.c6537f93.js
@@ -0,0 +1,2 @@
+console.log("Hello!");const t={startButton:document.querySelector("[data-start]"),stopButton:document.querySelector("[data-stop]")};t.startButton.addEventListener("click",(function(){})),t.stopButton.addEventListener("click",(function(){}));
+//# sourceMappingURL=01-color-switcher.c6537f93.js.map
