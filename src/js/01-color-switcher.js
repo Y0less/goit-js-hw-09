a25fc24148647747274ca35012ef2a9b6a6bf786
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-07-06 01:19:46 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,15 @@
 console.log('Hello!');
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
+
+// console.log('refs.startButton :>> ', refs.startButton);
+// console.log('refs.stopButton :>> ', refs.stopButton);
+
+refs.startButton.addEventListener('click', startColorCycle);
+refs.stopButton.addEventListener('click', stopColorSycle);
+
+function startColorCycle() {}
+
+function stopColorSycle() {}
