b3bf9d6cc569572425074073e29c24d5d3c132dd
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-07-07 22:30:54 +0300

    task-3 completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+// console.log('refs.form :>> ', refs.form);
+
+refs.form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: {
+      delay: { value: delay },
+      step: { value: step },
+      amount: { value: amount },
+    },
+  } = event.currentTarget;
+  // event.currentTarget.reset();
+  generatePromises(delay, step, amount);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function generatePromises(delay, step, amount) {
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i + 1, parseInt(delay) + i * parseInt(step))
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
